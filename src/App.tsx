82de1612bf2cb5bd02f6dc138405b65d350e0334
Илья Богачев <Nate-Death@Yandex.ru>
2021-05-18 15:44:09 +0400

    Add hooks, Add source file
    Add useState and useEffect imports to App.tsx
    Add Cities.json source file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 // eslint-disable-next-line no-use-before-define
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import WeatherCard from './components/WeatherCard'
+import Cities from './source/Cities.json'
 
 function App () {
+  const [lat, setLat] = useState([]);
+  const [lon, setLon] = useState([]);
   return (
     <div className="App">
       <WeatherCard dt={1602104400 * 1000} temp='24' icon='01d' />
