e20551cbdf907d52cea5f763267ea6741fe50002
Илья Богачев <Nate-Death@Yandex.ru>
2021-05-22 18:38:59 +0400

    Add custom hook
    Add custom hook
    Fix App.tsx
    Fix CitySelector.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
 // eslint-disable-next-line no-use-before-define
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import './App.css'
 import WeatherCard from './components/WeatherCard'
 import Cities from './source/Cities.json'
 import CitySelector from './components/CitySelector'
-
+import UseFetch from './hooks/UseFetch'
 function App () {
   type Key = keyof typeof Cities
-  const [lat, setLat] = useState<any>([])
-  const [lon, setLon] = useState<any>([])
-  const [data, setData] = useState<any>([])
-  const [city, SetCity] = useState<Key>('Samara')
-  useEffect(() => {
-    const fetchData = async () => {
-      SetCity('Kazan')
-      setLat(Cities[city].lat)
-      setLon(Cities[city].lon)
-      await fetch(`${process.env.REACT_APP_API_URL}lat=${lat}&lon=${lon}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-        .then(res => res.json())
-        .then(result => {
-          setData(result)
-          console.log(result)
-        })
-    }
-    fetchData()
-  }, [lat, lon])
+  const { data, error, isLoading, setUrl } = UseFetch('')
+  const handleChange = (e: any) => {
+    setUrl(`${process.env.REACT_APP_API_URL}lat=${Cities[e as Key].lat}&lon=${Cities[e as Key].lon}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+  }
+  const getContent = () => {
+    if (error) return <h2>Error when fetching: {error}</h2>
+    if (!data && isLoading) return <h2>LOADING...</h2>
+    if (!data) return <h1>placeholder</h1>
+    return <WeatherCard dt={data.current.dt * 1000} temp={data.current.temp} icon={data.current.weather[0].icon} />
+  }
   return (
     <div className="App">
-      {(typeof data.current !== 'undefined') ? (<WeatherCard dt={data.current.dt * 1000} temp={data.current.temp} icon={data.current.weather[0].icon} />) : (<div><CitySelector cities={Cities} /></div>)}
+     <div>
+      <CitySelector cities={Cities} handleChange={handleChange}/>
+      <div>
+      {getContent()}
+      </div>
+     </div>
+     <div>
+
+     </div>
     </div>
   )
 }
--- src/components/CitySelector.tsx
+++ src/components/CitySelector.tsx
@@ -3,15 +3,18 @@ import React from 'react'
 
 interface CitySelectorProps {
     cities:object,
+    handleChange : (e:any) => void
 }
 
 const CitySelector = (props : CitySelectorProps) => {
   const citiesArray = []
   for (const key in props.cities) {
-    citiesArray.push(key)
+    if (key !== 'empty') {
+      citiesArray.push(key)
+    }
   }
   return (
-    <select>
+    <select onChange={(event) => props.handleChange(event.target.value)}>
         <option value='empty' selected disabled hidden>Select city</option>
         {citiesArray.map(item => (
             <option key={item} value={item}>{item}</option>
